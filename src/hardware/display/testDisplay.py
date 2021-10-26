c622b97b87f98fa38b8b5cce078e346c7fd1d689
SimonKoll <simonkoll20@gmail.com>
2021-10-26 17:14:45 +0200

    new try on display

--- src/hardware/display/testDisplay.py
+++ src/hardware/display/testDisplay.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 
-
 from signal import signal, SIGTERM, SIGHUP, pause
 from rpi_lcd import LCD
 
 lcd = LCD()
 
+
 def safe_exit(signum, frame):
     exit(1)
 
 
-signal(SIGTERM, safe_exit)
-signal(SIGHUP, safe_exit)
-
 try:
-    lcd.text("Hello,",1)
-    lcd.text("Raspberry!",2)
+    signal(SIGTERM, safe_exit)
+    signal(SIGHUP, safe_exit)
+
+    lcd.text("Hello,", 1)
+    lcd.text("Raspberry Pi!", 2)
+
+    pause()
+
 except KeyboardInterrupt:
     pass
 
