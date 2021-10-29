92f6e727cac768e01df7bcd0fd5dc79ce9782ac3
SimonKoll <simonkoll20@gmail.com>
2021-10-29 15:37:32 +0200

    keypad v1

---
+++ src/hardware/numpad/mainNumpad.py
@@ -0,0 +1,20 @@
+import time
+import RPi.GPIO as GPIO
+from keypad4 import keypad
+
+kp = keypad(columnCount=4)
+digit = None
+last_digit = None
+print('[press ctrl+c to end the script]')
+try:  # Main program loop
+    while True:
+    digit = kp.getKey()
+if not last_digit == digit:
+    if not digit == None:
+    print("{}".format(digit))
+last_digit = digit
+time.sleep(0.5)
+# Scavenging work after the end of the program
+except KeyboardInterrupt:
+GPIO.cleanup()
+print('Srcipt end!')
