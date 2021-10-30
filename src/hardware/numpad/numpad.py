0bcd8e2fe2d39f3c2a604f7a7b1ad320588a8478
SimonKoll <simonkoll20@gmail.com>
2021-10-30 15:12:41 +0200

    indentations are crap

--- src/hardware/numpad/numpad.py
+++ src/hardware/numpad/numpad.py
@@ -32,15 +32,15 @@ class keypad():
             for i in range(len(self.ROW)):
                 GPIO.setup(self.ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
                 rowVal = -1
-                for i in range(len(self.ROW)):
-                    tmpRead = GPIO.input(self.ROW[i])
-                    if tmpRead == 0:
-                        rowVal = i
-                if rowVal < 0 or rowVal > 3:
-                    self.exit()
-                    return
-                for j in range(len(self.COLUMN)):
-                    GPIO.setup(self.COLUMN[j], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            for i in range(len(self.ROW)):
+                tmpRead = GPIO.input(self.ROW[i])
+                if tmpRead == 0:
+                    rowVal = i
+            if rowVal < 0 or rowVal > 3:
+                self.exit()
+                return
+            for j in range(len(self.COLUMN)):
+                GPIO.setup(self.COLUMN[j], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         GPIO.setup(self.ROW[rowVal], GPIO.OUT)
         GPIO.output(self.ROW[rowVal], GPIO.HIGH)
