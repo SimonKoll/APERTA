159ab5fa22db0c3bdf169215082d3c590b5e88c4
SimonKoll <simonkoll20@gmail.com>
2022-02-05 14:58:26 +0100

    displaying 5 character long combination on display

--- src/hardware/numpad/numpad.py
+++ src/hardware/numpad/numpad.py
@@ -1,14 +1,17 @@
 import RPi.GPIO as GPIO
 import time
+from  hardware.display.testDisplay import displayText
 
-GPIO.setmode(GPIO.board)
+GPIO.setmode(GPIO.BCM)
 
 MATRIX = [[1, 2, 3, 'A'],
 [4, 5, 6, 'B'],
 [7, 8, 9, 'C'],
-['*', 0, '#', 'D']
+['*', 0, '#', 'D']]
 
 
+COUNT = 0
+COMBIN = ""
 ROW = [18, 23, 24, 25]
 COL = [12, 16, 20, 21]
 
@@ -20,12 +23,16 @@ for i in range(4):
     GPIO.setup(ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
-    while(True)
+    while(True):
         for j in range(4):
             GPIO.output(COL[j], 0)
             for i in range(4):
                 if GPIO.input(ROW[i]) == 0:
-                    print MATRIX[i][j]
+                    print(MATRIX[i][j])
+                    COUNT = COUNT+1
+                    COMBIN = COMBIN + MATRIX[i][j]
+                    if(COUNT > 5):
+                        displayText(COMBIN)
                     while(GPIO.input(ROW[i]) == 0):
                         pass
                         time.sleep(0.2)
