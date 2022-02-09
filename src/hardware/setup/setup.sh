4c1044085d2205a6c9e3a74b65f2261074ca2ddc
SimonKoll <simonkoll20@gmail.com>
2022-02-09 08:31:37 +0100

    startet working on rest API

--- src/hardware/setup/setup.sh
+++
@@ -1,5 +0,0 @@
-pip3 install easyocr --user
-pip3 install imutils --user
-pip install opencv-python --user
-pip install numpy --user
-pip install matplotlib --user
---
+++ src/server/server.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.set('view engine', 'ejs')
+const MongoClient = require('mongodb').MongoClient
+connstring = 'mongodb+srv://aperta:SiDaBe2021@cluster0.xh3v9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function(req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+MongoClient.connect(connstring, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('ApertaDB')
+        const licensplateCollection = db.collection('licenseplate')
+        const rfidCollection = db.collection('rfid')
+        const keypadCollection = db.collection('keypad')
+            // All your handlers here...
+
+        app.delete('/delete-entrants', (req, res) => {
+            entrantCollection.findOneAndDelete({ "id": req.body(id) })
+        })
+        app.post('/licenseplate', (req, res) => {
+            licensplateCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+        app.post('/keypad', (req, res) => {
+            keypadCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+        app.post('/rfid', (req, res) => {
+                rfidCollection.insertOne(req.body)
+                    .then(result => {
+                        res.redirect('/')
+                    })
+                    .catch(error => console.error(error))
+            })
+            // We normally abbreviate `request` to `req` and `response` to `res`.
+        app.get('/get-keypad-codes', function(req, res) {
+            db.collection('keypad').find().toArray()
+                .then(results => {
+                    console.log("retrieving Entrants successful")
+                    res.send(results)
+                })
+                .catch(error => console.error(error))
+                // do something here
+        })
+        app.get('/get-licenseplates', function(req, res) {
+            db.collection('licenseplate').find().toArray()
+                .then(results => {
+                    console.log("retrieving unknown Entrants")
+                    res.send(results)
+                })
+                .catch(error => console.error(error))
+                // do something here
+        })
+        app.get('/get-rfid-codes', function(req, res) {
+            db.collection('rfid').find().toArray()
+                .then(results => {
+                    console.log("retrieving unknown Entrants")
+                    res.send(results)
+                })
+                .catch(error => console.error(error))
+                // do something here
+        })
+    }).catch(console.error)
+
+
+
+
+app.listen(3000, function() {
+    console.log('listening on 3000')
+})
