6c952196812267d54447b07a6ca1b6e04667799c
SimonKoll <simonkoll20@gmail.com>
2022-03-06 16:08:32 +0100

    worked on server

--- src/hardware/setup_licenseplate.sh
+++ src/hardware/setup_licenseplate.sh
@@ -1,9 +1,9 @@
-pip3 install opencv-python==3.4.2
+pip3 install opencv-python
 
-pip3 install numpy==1.17.2
+pip3 install numpy
 
-pip3 install skimage==0.16.2
+pip3 install skimage
 
-pip3 install tensorflow==1.15.0
+pip3 install tensorflow
 
-pip3 install imutils==0.5.3
+pip3 install imutils
--- src/server/server.js
+++ src/server/server.js
@@ -4,23 +4,17 @@ const app = express()
 app.set('view engine', 'ejs')
 const MongoClient = require('mongodb').MongoClient
 connstring = 'mongodb+srv://aperta:SiDaBe2021@cluster0.xh3v9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(function(req, res, next) {
-
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
     // Pass to next layer of middleware
     next();
 });
@@ -32,36 +26,40 @@ MongoClient.connect(connstring, { useUnifiedTopology: true })
         const rfidCollection = db.collection('rfid')
         const keypadCollection = db.collection('keypad')
             // All your handlers here...
-
-        app.delete('/delete-entrants', (req, res) => {
-            entrantCollection.findOneAndDelete({ "id": req.body(id) })
+        app.delete('/delete-rfid', (req, res) => {
+            rfidCollection.findOneAndDelete({ "chip_id": req.body(id) })
         })
-        app.post('/licenseplate', (req, res) => {
+        app.delete('/delete-keypad', (req, res) => {
+            keypadCollection.findOneAndDelete({ "pi": req.body(id) })
+        })
+        app.delete('/delete-licenseplate', (req, res) => {
+            licensplateCollection.findOneAndDelete({ "licenseplate": req.body(id) })
+        })
+        app.post('/add-licenseplate', (req, res) => {
             licensplateCollection.insertOne(req.body)
                 .then(result => {
                     res.redirect('/')
                 })
                 .catch(error => console.error(error))
         })
-        app.post('/keypad', (req, res) => {
+        app.post('/add-keypad', (req, res) => {
             keypadCollection.insertOne(req.body)
                 .then(result => {
                     res.redirect('/')
                 })
                 .catch(error => console.error(error))
         })
-        app.post('/rfid', (req, res) => {
-                rfidCollection.insertOne(req.body)
-                    .then(result => {
-                        res.redirect('/')
-                    })
-                    .catch(error => console.error(error))
-            })
-            // We normally abbreviate `request` to `req` and `response` to `res`.
+        app.post('/add-rfid', (req, res) => {
+            rfidCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
         app.get('/get-keypad-codes', function(req, res) {
             db.collection('keypad').find().toArray()
                 .then(results => {
-                    console.log("retrieving Entrants successful")
+                    console.log("retrieving keypad codes")
                     res.send(results)
                 })
                 .catch(error => console.error(error))
@@ -70,7 +68,7 @@ MongoClient.connect(connstring, { useUnifiedTopology: true })
         app.get('/get-licenseplates', function(req, res) {
             db.collection('licenseplate').find().toArray()
                 .then(results => {
-                    console.log("retrieving unknown Entrants")
+                    console.log("retrieving licenseplates")
                     res.send(results)
                 })
                 .catch(error => console.error(error))
@@ -79,16 +77,15 @@ MongoClient.connect(connstring, { useUnifiedTopology: true })
         app.get('/get-rfid-codes', function(req, res) {
             db.collection('rfid').find().toArray()
                 .then(results => {
-                    console.log("retrieving unknown Entrants")
+                    console.log("retrieving rfid codes")
                     res.send(results)
                 })
                 .catch(error => console.error(error))
                 // do something here
         })
+
+
     }).catch(console.error)
-
-
-
 
 app.listen(3000, function() {
     console.log('listening on 3000')
