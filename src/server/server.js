357302bba8b50cbdcd849be1b63a2110c40051ce
SimonKoll <simonkoll20@gmail.com>
2022-03-06 17:09:13 +0100

    server and thesis

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ connstring = 'mongodb+srv://aperta:SiDaBe2021@cluster0.xh3v9.mongodb.net/myFirst
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(function(req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
