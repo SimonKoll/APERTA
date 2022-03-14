e4b3e20a309cad1bf2d6a617d418bba92126726e
SimonKoll <simonkoll20@gmail.com>
2022-03-14 08:36:05 +0100

    server and images

--- src/server/test.js
+++ src/server/test.js
@@ -1,16 +1,10 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://aperta:SiDaBe2021@apertadb.tdbak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://aperta:SiDaBe2021@apertadb.tdbak.mongodb.net/ApertaDB?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-    const collection = client.db("ApertaDB").collection("lincensplate").find().toArray()
-        .then(results => {
-            console.log("retrieving licenseplates")
-            res.send(results)
-        })
-        .catch(error => console.error(error))
-        // perform actions on the collection object
+    console.log("open connection")
+    console.log(client)
+    console.log("finding collection")
+    const coll = client.db("ApertaDB").collection("licenseplate");
     client.close();
 });
