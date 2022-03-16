73f77457d672a9ca035ba0703ac62db68c7c2ab5
Benjamin Golic <golicbenjamin@gmx.at>
2022-03-16 09:24:03 +0100

    fixed hr on about page

--- src/webshop/aperta-shop/src/components/About/About.jsx
+++ src/webshop/aperta-shop/src/components/About/About.jsx
@@ -10,13 +10,16 @@ const About = () => {
             <div className="about-wrapper">
                 <div className="top-wrapper">
                     <span className="aboutAPERTA-title">About APERTA</span>
-                    <span className="about-desc">
-                        APERTA solves a problem that is familiar to everyday life. The garage transmitter has been misplaced, the battery has gone 
-    flat, or you want to enter the garage without your car. Until now, you had to buy and install several systems for this. APERTA 
-    combines number plate recognition with a traditional number field, as well as the possibility of using RFID chips. The 
-    number plate recognition takes a picture of the surroundings, searches for the number plate on it, and if it has been added 
-    beforehand via the user interface, the garage door is activated and you can enter.
-                    </span>
+                    <div className="about-desc-wrapper">
+                        <span className="about-desc">
+                            APERTA solves a problem that is familiar to everyday life. The garage transmitter has been misplaced, the battery has gone 
+        flat, or you want to enter the garage without your car. Until now, you had to buy and install several systems for this. APERTA 
+        combines number plate recognition with a traditional number field, as well as the possibility of using RFID chips. The 
+        number plate recognition takes a picture of the surroundings, searches for the number plate on it, and if it has been added 
+        beforehand via the user interface, the garage door is activated and you can enter.
+                        </span>
+                    </div>
+
                     <div className="button-wrapper">
                         <div className="aboutAPERTA-button">
                             <div className="aboutAPERTA-btn-content" >
